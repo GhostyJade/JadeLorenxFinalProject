95c4892710df91b6143a25265769b1429f76bc70
GhostyJade <giandreas@outlook.it>
2020-05-21 22:41:44 +0200

    Updated view appearence, fixed text localization

--- frontend/Hellocate/views/RegistrationView.js
+++ frontend/Hellocate/views/RegistrationView.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-import { View, TextInput, Button } from 'react-native'
+import { View, TextInput, Button, Text } from 'react-native'
 
 import { useTracked } from '../configs/global_state'
 
 import * as Config from '../configs/index'
 
+import i18n from 'i18n-js'
+
 export default function RegistrationView() {
+
+    const [registrationData, setRegistrationData] = React.useState({ username: '', password: '', confirmPassword: '' })
 
     const [state, dispatch] = useTracked()
 
@@ -14,17 +18,33 @@ export default function RegistrationView() {
         fetch(Config.Network.serverURI + '/users/', {
             method: 'POST',
             body: {
-                
+
             }
         })
     }
 
+    const updateUsernameText = (e) => {
+        setRegistrationData({ ...registrationData, username: e })
+    }
+
+    const updatePasswordText = (e) => {
+        setRegistrationData({ ...registrationData, password: e })
+    }
+
+    const updateConfirmPasswordText = (e) => {
+        setRegistrationData({ ...registrationData, confirmPassword: e })
+    }
+
     return (
-        <View>
-            <TextInput placeholder="Username" />
-            <TextInput placeholder="Password" secureTextEntry />
-            <TextInput placeholder="Confirm password" secureTextEntry />
-            <Button title="Register" />
+        
+            <View style={Config.Styles.RegistrationViewStyles.container}>
+            <Text style={Config.Styles.RegistrationViewStyles.welcome} >{i18n.t('registrationWelcome')}</Text>
+                <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Username" key="username" value={registrationData.username} onChangeText={updateUsernameText} />
+                <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Password" secureTextEntry key="password" value={registrationData.password} onChangeText={updatePasswordText} />
+                <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Confirm password" secureTextEntry key="confirmPassword" value={registrationData.confirmPassword} onChangeText={updateConfirmPasswordText} />
+                <View style={Config.Styles.RegistrationViewStyles.confirmButton}>
+                    <Button color="#a800ff" title="Register" />
+                </View>
         </View>
     )
 
