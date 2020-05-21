677f29462f96c8b2a30ebaf45c97516074fa97bf
GhostyJade <giandreas@outlook.it>
2020-05-21 18:59:45 +0200

    Renamed file

--- frontend/Hellocate/views/RegistrationView.js
+++ frontend/Hellocate/views/RegistrationView.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React from "react"
 
 import { View, TextInput, Button } from 'react-native'
 
 import { useTracked } from '../configs/global_state'
 
+import * as Config from '../configs/index'
 
 export default function RegistrationView() {
 
     const [state, dispatch] = useTracked()
 
+    const performRegistration = () => {
+        fetch(Config.Network.serverURI + '/users/', {
+            method: 'POST',
+            body: {
+                
+            }
+        })
+    }
+
     return (
         <View>
             <TextInput placeholder="Username" />
             <TextInput placeholder="Password" secureTextEntry />
+            <TextInput placeholder="Confirm password" secureTextEntry />
             <Button title="Register" />
         </View>
     )
