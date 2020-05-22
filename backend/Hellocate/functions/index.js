b817beca750d356273e6656a1bc465ea8276fb23
GhostyJade <giandreas@outlook.it>
2020-05-22 17:27:28 +0200

    Moved registration and login to separate files, moved users config

--- backend/Hellocate/functions/index.js
+++ backend/Hellocate/functions/index.js
@@ -17,7 +17,6 @@ main.use(express.json())
 
 // Creates the database reference
 const db = admin.database()
-const usersCollection = "users/"
 
 // Import the config file
 const Config = require('./config.json')
@@ -26,55 +25,14 @@ const Config = require('./config.json')
 const { DataUtils } = require('./utils/datautils')
 const DataUtilities = new DataUtils(db)
 
-// Import net functions
-
+// Import app functions routes
+const User = require('./routes/user/index')
 
 //login
-app.post('/users/:username', async (req, res) => {
-    const { username } = req.params
-    const { password } = req.body
-
-    let authenticated = false
-    let token = null
-
-    let user = undefined
-
-    await db.ref(`${usersCollection}/${username}/`).once("value", snapshot => {
-        if (snapshot.exists()) {
-            user = snapshot.val()
-        }
-    })
-
-    if (user) {
-        authenticated = await crypt.compare(password, user.password)
-        if (authenticated) {
-            token = jwt.sign({ username }, Config.SECRET_KEY, { expiresIn: '24h' })
-        }
-    }
-    res.send({ authenticated, token })
-})
+User.Login(app, Config, db, crypt, jwt)
 
 // register
-app.post('/users/', async (req, res) => {
-
-    const { username, usrPassword } = req.body.data
-    const password = await crypt.hash(usrPassword, 8)
-
-    let registered = false
-
-    //this check if the username field exists and verify if the specified value exists in the db
-    await db.ref(`${usersCollection}/${username}/username`).once("value", snapshot => {
-        if (snapshot.exists()) {
-            if (snapshot.val() === username)
-                registered = true
-        }
-    })
-
-    if (!registered)
-        await db.ref(usersCollection + username).set({ username, password })
-
-    res.send({ registered: !registered })
-})
+User.Register(app, Config, db, crypt)
 
 // export the function used by firebase
 exports.api = functions.https.onRequest(main) //Note: to call this api you must use {baseurl}/api/v1/{function}
