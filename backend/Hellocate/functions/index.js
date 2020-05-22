f7088668fb27421be69aa834f2fdeee67c709252
GhostyJade <giandreas@outlook.it>
2020-05-22 14:52:46 +0200

    Implemented registration with firebase

--- backend/Hellocate/functions/index.js
+++ backend/Hellocate/functions/index.js
@@ -12,17 +12,29 @@ const main = express()
 main.use('/v1/', app)
 main.use(express.json())
 
-const db = admin.firestore()
-const userCollection = 'users'
+const db = admin.database()
+const usersCollection = "users/"
 
+// register
 app.post('/users/', async (req, res) => {
 
     const { username, usrPassword } = req.body.data
     const password = await crypt.hash(usrPassword, 8)
 
-    const result = await (db.collection(userCollection)).add({ username, password })
+    let registered = false
 
-    res.send({ result, registered: true }) //check for errors, check if user already exists
+    //this check if the username field exists and verify if the specified value exists in the db
+    await db.ref(`${usersCollection}/${username}/username`).once("value", snapshot => {
+        if (snapshot.exists()) {
+            if (snapshot.val() === username)
+                registered = true
+        }
+    })
+
+    if (!registered)
+        await db.ref(usersCollection + username).set({ username, password })
+
+    res.send({ registered: !registered })
 })
 
 exports.api = functions.https.onRequest(main)
