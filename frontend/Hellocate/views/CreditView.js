dd2d1855e13f9820a86504d32608a6d1f728a0ff
GhostyJade <giandreas@outlook.it>
2020-05-23 16:14:19 +0200

    Removed useless log

--- frontend/Hellocate/views/CreditView.js
+++ frontend/Hellocate/views/CreditView.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { StyleSheet, Text, Button, View } from 'react-native'
 
 export default function CreditView(props) {
-    console.log(props.visibility)
     return (
         <View style={styles.creditOverlay}>
             <Text>App made by</Text>
