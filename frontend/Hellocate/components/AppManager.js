443272dba573031781706b804d21b629453d59fd
GhostyJade <giandreas@outlook.it>
2020-05-25 19:58:56 +0200

    Rewrote routing, code cleanup

--- frontend/Hellocate/components/AppManager.js
+++ frontend/Hellocate/components/AppManager.js
@@ -1,50 +1,22 @@
 import React from 'react'
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
-import { HomeView } from '../views/index'
-import { Text } from 'react-native'
-
 import { useTracked } from '../configs/global_state'
 
-import { NavigationContainer } from '@react-navigation/native'
+import { Router, Stack, Scene } from "react-native-router-flux"
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import RegistrationView from '../views/RegistrationView'
-import NewAmbientView from '../views/NewAmbientView'
-
-
-import * as Config from '../configs/index'
-
-const Tab = createBottomTabNavigator()
-
-const UserLoggedInView = () => {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName
-                    if (route.name === 'Home')
-                        iconName = focused ? 'home' : 'home-outline'
-                    return <Icon name={iconName} style={Config.Styles.AppNavigatorStyles.tabNavigatorIcon} />
-                },
-                tabBarLabel: ({ focused, color }) => {
-                    return <Text style={Config.Styles.AppNavigatorStyles.tabNavigatorText}>{route.name}</Text>
-                }
-            })} >
-                <Tab.Screen name="Home" component={HomeView} key="home"></Tab.Screen>
-            </Tab.Navigator>
-        </NavigationContainer >
-    )
-}
+import * as Views from '../views/index'
 
 export default function AppManager() {
 
     const [state, dispatch] = useTracked()
 
     return (
-        <>
-            {state.authenticated ? (state.showNewAmbientView ? <NewAmbientView /> : <UserLoggedInView />) : <RegistrationView />}
-        </>
+        <Router >
+            <Stack >
+                <Scene hideNavBar key="home" component={Views.RegistrationView} />
+                <Scene hideNavBar key="login" component={Views.LoginView} />
+                <Scene hideNavBar key="ambient" component={Views.NewAmbientView} />
+            </Stack>
+        </Router>
     )
 }
