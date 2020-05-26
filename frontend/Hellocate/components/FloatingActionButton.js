0edd6ec6aa4b173eb27f48e32721c52dbe0df801
GhostyJade <giandreas@outlook.it>
2020-05-26 14:38:47 +0200

    Updated fab

--- frontend/Hellocate/components/FloatingActionButton.js
+++ frontend/Hellocate/components/FloatingActionButton.js
@@ -3,12 +3,11 @@ import React from 'react'
 import { FAB } from 'react-native-paper'
 
 import * as Config from '../configs/index'
+import { Actions } from 'react-native-router-flux'
 
 export default function FloatingActionButton(props) {
 
-    const [state, dispatch] = Config.GlobalState.useTracked()
-
     return (
-       <FAB icon="plus" style={Config.Styles.FloatingActionButtonStyles.fab}  onPress={() => dispatch({ type: 'showAmbientView' })}/>
+       <FAB icon="plus" style={Config.Styles.FloatingActionButtonStyles.fab}  onPress={() => Actions.ambient()}/>
     )
 }
