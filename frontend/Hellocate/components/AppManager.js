252b2ac38c65da28fca97a5b823159cda417da11
GhostyJade <giandreas@outlook.it>
2020-05-26 14:39:19 +0200

    Recreated routes

--- frontend/Hellocate/components/AppManager.js
+++ frontend/Hellocate/components/AppManager.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
-import { useTracked } from '../configs/global_state'
-
 import { Router, Stack, Scene } from "react-native-router-flux"
 
 import * as Views from '../views/index'
 
 export default function AppManager() {
 
-    const [state, dispatch] = useTracked()
-
     return (
         <Router >
             <Stack >
-                <Scene hideNavBar key="home" component={Views.RegistrationView} />
                 <Scene hideNavBar key="login" component={Views.LoginView} />
+                <Scene hideNavBar key="registration" component={Views.RegistrationView} />
+                <Scene hideNavBar key="home" component={Views.HomeView} />
                 <Scene hideNavBar key="ambient" component={Views.NewAmbientView} />
             </Stack>
         </Router>
