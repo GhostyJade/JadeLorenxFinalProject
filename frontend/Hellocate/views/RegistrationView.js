a26d3b244f9809e33721c9b7eca858f69babf6c5
GhostyJade <giandreas@outlook.it>
2020-05-26 14:40:09 +0200

    Finished working on RegistrationView

--- frontend/Hellocate/views/RegistrationView.js
+++ frontend/Hellocate/views/RegistrationView.js
@@ -7,21 +7,49 @@ import { useTracked } from '../configs/global_state'
 import * as Config from '../configs/index'
 
 import i18n from 'i18n-js'
+import { Snackbar } from "react-native-paper"
+import { Actions } from 'react-native-router-flux'
 
 export default function RegistrationView() {
 
-    const [registrationData, setRegistrationData] = React.useState({ username: '', password: '', confirmPassword: '' })
-
     const [state, dispatch] = useTracked()
 
-    const performRegistration = () => {
-        //check data
-        fetch(Config.Network.serverURI + '/users/', {
-            method: 'POST',
-            body: {
+    const [registrationData, setRegistrationData] = React.useState({ username: '', password: '', confirmPassword: '' })
+    const [snackbar, setSnackbar] = React.useState({ active: false, message: '' })
 
+    const dismissSnackbar = () => {
+        setSnackbar({ ...snackbar, active: !snackbar.active })
+    }
+
+    const performRegistration = () => {
+        if (registrationData.username === '' || registrationData.password === '' || registrationData.confirmPassword === '') {
+            setSnackbar({ active: true, message: 'snackbar_allFieldsRequired' })
+            return;
+        }
+        if (registrationData.password !== registrationData.confirmPassword) {
+            setSnackbar({ active: true, message: 'registration_passwordNotMatch' })
+            return;
+        }
+
+        fetch(Config.Network.serverURI + "/" + Config.Network.apiPath + 'users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: {
+                    username: registrationData.username,
+                    usrPassword: registrationData.password
+                }
+            })
+        }).then(response => response.json()).then(result => {
+            if (result.registered) {
+                Actions.login()
+            } else {
+                setSnackbar({ active: true, message: 'registration_usernameTaken' })
+                return;
             }
-        }).then(response => response.json()).then()
+        })
     }
 
     const updateUsernameText = (e) => {
@@ -39,12 +67,18 @@ export default function RegistrationView() {
     return (
         <View style={Config.Styles.RegistrationViewStyles.container}>
             <Text style={Config.Styles.RegistrationViewStyles.welcome} >{i18n.t('registrationWelcome')}</Text>
-            <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Username" key="username" value={registrationData.username} onChangeText={updateUsernameText} />
+            <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder={i18n.t('registration_txtUsername')} key="username" value={registrationData.username} onChangeText={updateUsernameText} />
             <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Password" secureTextEntry key="password" value={registrationData.password} onChangeText={updatePasswordText} />
-            <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder="Confirm password" secureTextEntry key="confirmPassword" value={registrationData.confirmPassword} onChangeText={updateConfirmPasswordText} />
-            <View style={Config.Styles.RegistrationViewStyles.confirmButton}>
-                <Button color="#a800ff" title="Register" />
+            <TextInput style={Config.Styles.RegistrationViewStyles.inputField} placeholder={i18n.t('registration_txtConfirmPassword')} secureTextEntry key="confirmPassword" value={registrationData.confirmPassword} onChangeText={updateConfirmPasswordText} />
+            <View style={Config.Styles.RegistrationViewStyles.buttonGroup}>
+                <View style={Config.Styles.RegistrationViewStyles.buttonWrapper}>
+                    <Button color="#a800ff" title={i18n.t('registration_btnRegister')} onPress={performRegistration} />
+                </View>
+                <View style={Config.Styles.RegistrationViewStyles.buttonWrapper}>
+                    <Button color="#a800ff" title={i18n.t('registration_alreadyRegistered')} onPress={() => Actions.replace('login')} />
+                </View>
             </View>
+            <Snackbar onDismiss={dismissSnackbar} action={{ label: i18n.t('dismiss_snackbar'), onPress: dismissSnackbar }} visible={snackbar.active}>{i18n.t(snackbar.message)}</Snackbar>
         </View>
     )
 
